7f1f8a6f47823ebc2e6cba043d43d13cd8f31514
dongxin <zdx6653@gmail.com>
2018-07-15 08:31:30 -0700

    first commit

---
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -0,0 +1,101 @@
+package com.laioffer.eventreporter;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
+
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+
+
+//    /**
+//     * A dummy function to get fake event names.
+//     *
+//     * @return an array of fake event names.
+//     */
+//    private String[] getEventNames() {
+//        String[] names = {
+//                "Event1", "Event2", "Event3",
+//                "Event4", "Event5", "Event6",
+//                "Event7", "Event8", "Event9",
+//                "Event10", "Event11", "Event12"};
+//        return names;
+//    }
+//
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Life cycle test", "We are at onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Life cycle test", "We are at onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Life cycle test", "We are at onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Life cycle test", "We are at onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Life cycle test", "We are at onDestroy()");
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
